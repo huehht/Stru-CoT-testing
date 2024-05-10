439c964fb096cf562c4295b981e697bd398a4a07
huehht <1420356569@qq.com>
2024-05-10 11:01:14 +0800

    codingTest

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ from langchain_core.output_parsers import StrOutputParser
 llm = Ollama(model="llama3")
 output_parser = StrOutputParser()
 prompt = ChatPromptTemplate.from_messages([
-    ("system", "写一段代码以完成这个函数"),
+    ("system", "写一段代码以完成这个函数,不要有其他文字，并使用中文回答问题"),
     ("user", "{input}")
 ])
+print(prompt)
 chain = prompt | llm 
 out=chain.invoke({"input":"实现了飞行的起飞控制"})
 print(out)
